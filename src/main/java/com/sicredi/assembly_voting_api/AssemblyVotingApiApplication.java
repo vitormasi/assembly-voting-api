94dea3306fb2247cb6620433e9ea942a99813053
Vitor Macedo Silva <vitormasi@gmail.com>
2026-03-18 13:40:14 -0300

    init commit

---
+++ src/main/java/com/sicredi/assembly_voting_api/AssemblyVotingApiApplication.java
@@ -0,0 +1,13 @@
+package com.sicredi.assembly_voting_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssemblyVotingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssemblyVotingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sicredi/assembly_voting_api/AssemblyVotingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sicredi.assembly_voting_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssemblyVotingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
